bbd576e4954b495157ec18758f6e2d487c62514d
Aaron <devilfruits1999@gmail.com>
2019-10-15 10:51:23 -0400

    First Commit

---
+++ lab2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string runAgain = "y";
+
+            while (runAgain == "yes" || runAgain == "Yes" || runAgain == "y")
+            {
+                //input - gets the length, width, and height from the user
+                Console.Write("Enter length: ");
+                decimal classLength = decimal.Parse(Console.ReadLine());
+
+                Console.Write("Enter width: ");
+                decimal classWidth = decimal.Parse(Console.ReadLine());
+
+                Console.Write("Enter Height:");
+                decimal classHeight = decimal.Parse(Console.ReadLine());
+
+                //process - calculate the perimeter, area and volume of the room
+                decimal roomPerimeter = 2*(classLength + classWidth);
+                decimal roomArea = classLength * classWidth;
+                decimal roomVolume = roomArea * classHeight;
+
+                //output - display the perimeter, length and volume of the room
+                Console.WriteLine("Perimeter: " + roomPerimeter);
+                Console.WriteLine("Area: " + roomArea);
+                Console.WriteLine("Volume: " + roomVolume);
+
+                //check if we want to loop back
+                Console.Write("Continue?(y/n)");
+                runAgain = Console.ReadLine();
+            }
+
+        }
+    }
+}
